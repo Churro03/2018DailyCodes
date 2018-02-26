8d8f3cd22533c570fa3eb6a5b5dfb314cd78781b
Churro03 <35495503+Churro03@users.noreply.github.com>
2018-02-26 08:33:02 -0700

    Add files via upload

---
+++ 2-26.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<allegro5/allegro.h>
+#include<allegro5\allegro_primitives.h>
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	al_init();
+	al_init_primitives_addon();
+	ALLEGRO_DISPLAY *display = al_create_display(800, 800);
+
+	while (1) {
+		al_draw_filled_circle(rand() % 800, rand() % 800, rand() % 200, al_map_rgb(rand() % 256, 0, rand() % 256));
+		al_rest(.02);
+		al_flip_display();
+	}
+
+	al_destroy_display(display);
+
+}
