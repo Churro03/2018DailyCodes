9e2fef0451e56a6e7599f8efcab5c5ec529a83ef
Churro03 <35495503+Churro03@users.noreply.github.com>
2018-02-27 09:54:13 -0700

    Add files via upload

---
+++ 2-27.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main() {
+
+	string line;
+	ofstream myfile("duck.txt");
+	myfile << "your ducks are \"bad\" good " << endl;
+	myfile << "your ducks are nice" << endl;
+	myfile.close();
+	ifstream myfile1("duck.txt");
+	int input;
+	cin >> input;
+	if (myfile1.is_open())
+	{
+		while (getline(myfile1, line))
+		{
+			for (int i = 0; i < input; i++)
+				cout << line << endl;
+		}
+		myfile1.close();
+	}
+	else
+		cout << "unableto open file";
+	system("pause");
+}
